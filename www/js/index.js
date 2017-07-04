57f1e8f030b2c45fdc5de844ba8157d35e773720
Galih Aris Saputra <galih.ristra@gmail.com>
2017-07-04 11:48:24 +0700

    Part 5 Complete
    adding detail page

--- www/js/index.js
+++ www/js/index.js
@@ -18,7 +18,28 @@
  */
 $('#reposHome').bind('pageinit', function (event) {
     loadRepos();
-})
+});
+
+$('body').on('pageshow', '#reposDetail', function (event) {
+    var owner = getUrlVars().owner;
+    var name = getUrlVars().name;
+    loadRepoDetail(owner, name);
+});
+
+function loadRepoDetail(owner, name){
+    $.ajax("https://api.github.com/repos/" + owner + "/" + name).done(function(data) {
+        console.log(data);
+        
+        var repo = data;
+        
+        $('#repoName').html("<a href='" + repo.homepage + "'>" + repo.name + "</a>");
+        $('#description').text(repo.description);
+        $('#forks').html("<strong>Forks:</strong>" + repo.forks + "<br><strong>Watchers:</strong>" + repo.watchers);
+        
+        $('#avatar').attr('src', repo.owner.avatar_url);
+        $('#ownerName').html("<strong>Owner:</strong> <a href='" + repo.owner.url + "'>" + repo.owner.login + "</a>");
+    })
+}
 
 function loadRepos() {
     $.ajax("https://api.github.com/legacy/repos/search/javascript").done(function (data) {
@@ -30,4 +51,15 @@ function loadRepos() {
         });
         $('#allRepos').listview('refresh');
     });
+}
+
+function getUrlVars() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++){
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
 }
